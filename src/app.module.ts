6f57966e12c46b662c34fd0c9ef679af5142f9b4
Vo Tran Phi <songokupkj@gmail.com>
2024-10-30 01:58:24 +0700

    feat: new user's MVC model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '192.168.130.129',
+      port: 5432,
+      password: 'mypassword',
+      username: 'myuser',
+      entities: [],
+      database: 'mydatabase',
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
 export class AppModule {}
