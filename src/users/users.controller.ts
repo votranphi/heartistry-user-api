f1d79b08e326c8808088f10a01c39f044075e616
Vo Tran Phi <songokupkj@gmail.com>
2024-12-07 00:17:25 +0700

    fix: don't return the password after signing up

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,7 +8,7 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { SignUpDto } from './dto/sign-up.dto';
-import { User } from './entities/user.entity';
+import { UserInfoDto } from './dto/user-info.dto';
 
 @Controller('users')
 @UsePipes(
@@ -26,7 +26,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('signup')
-  async create(@Body() signUpDto: SignUpDto): Promise<User> {
+  async create(@Body() signUpDto: SignUpDto): Promise<UserInfoDto> {
     if (await this.usersService.findUserByUsername(signUpDto.username)) {
       throw new HttpException('Used Username', 400);
     }
