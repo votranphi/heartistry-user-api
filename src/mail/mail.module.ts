6b8cc65b81739a4e966ca7dc8b23ba3ad1a94e6d
Vo Tran Phi <songokupkj@gmail.com>
2024-12-08 11:15:16 +0700

    refactor: change sender's name

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -20,7 +20,7 @@ import { ConfigService } from '@nestjs/config';
           },
         },
         defaults: {
-          from: `"No Reply" <${config.get('MAIL_FROM')}>`,
+          from: `"Heartistry Service" <${config.get('MAIL_FROM')}>`,
         },
         template: {
           dir: join(__dirname, 'mail', 'templates'),
