07dd35c7bfb0ca22efe971226f9beb172b674a59
Vo Tran Phi <songokupkj@gmail.com>
2024-12-22 23:33:36 +0700

    refactor: change auth's flow

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,11 +1,10 @@
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy } from 'passport-local'
-import { AuthService } from "../auth.service";
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-    constructor(private authService: AuthService) {
+    constructor() {
         super();
     }
 
